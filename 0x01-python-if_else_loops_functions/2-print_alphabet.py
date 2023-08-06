6878039b693d58be225347408207931cde9b465a
SGXaba <sgxaba7@gmail.com>
2023-08-05 23:05:46 -0700

    python if else loops function

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    tableAsciiToAlphabet = chr(i)
-    asciiToAlpha = "" + tableAsciiToAlphabet
-    print("{}".format(asciiToAlpha), end='')
+    print("{:c}".format(i), end='')
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i == 99:
-        print(i)
-else:
-    print(f"{}".format('0' + str(i) if i < 10 else i), end=", ")
+for digit1 in range(0, 9):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8:
+            print("{:d}{:d}".format(digit1, digit2))
+            break
+        print("{:d}{:d}".format(digit1, digit2), end=", ")
